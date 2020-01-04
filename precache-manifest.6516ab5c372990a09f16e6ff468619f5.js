9d4da1cb81e09268b93b33179f5a05d36f81e967
abhijtih <abhijith@immenseit.com>
2020-01-04 14:50:34 +0530

    Updates

---
+++ precache-manifest.6516ab5c372990a09f16e6ff468619f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f24b9ba9ed6365869470b444aba3ef62",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "98224f1c5704adca32ea",
+    "url": "/react-gh-pages/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "af884d105b41fa750913",
+    "url": "/react-gh-pages/static/js/2.82e1ef5a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-gh-pages/static/js/2.82e1ef5a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "98224f1c5704adca32ea",
+    "url": "/react-gh-pages/static/js/main.1e000488.chunk.js"
+  },
+  {
+    "revision": "6ddad23638b2a65d42d8",
+    "url": "/react-gh-pages/static/js/runtime-main.663af488.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
